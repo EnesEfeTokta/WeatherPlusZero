8ab035c6989a46607306d87914a45613fe8d4f3b
Enes Efe Tokta <enesefetokta009@gmail.com>
2025-02-01 23:09:12 +0300

    The conformity format of the information entered on the input screen was checked.

--- WeatherPlusZero/Codes/Business/UserSessionManager.cs
+++ WeatherPlusZero/Codes/Business/UserSessionManager.cs
@@ -12,10 +12,49 @@ namespace WeatherPlusZero
     {
         private DataBase dataBase;
         private NotificationManagement notificationManagement;
+        private AuthenticationValidator authenticationValidator;
+
         public UserManager() 
         {
             dataBase = new DataBase();
             notificationManagement = new NotificationManagement();
+            authenticationValidator = new AuthenticationValidator();
+        }
+
+        public bool ValidateFieldsContinuously(ValidateType validateType, string value)
+        {
+            switch (validateType)
+            {
+                case ValidateType.NameSurname:
+                    if (!authenticationValidator.ValidateNameSurname(value))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                case ValidateType.Email:
+                    if (!authenticationValidator.ValidateEmail(value))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                case ValidateType.Password:
+                    if (!authenticationValidator.ValidatePassword(value))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+            }
+
+            return true;
         }
 
         public async void LogIn(User user)
@@ -83,6 +122,55 @@ namespace WeatherPlusZero
         }
     }
 
+    public class AuthenticationValidator
+    {
+        private readonly char[] specialCharacters = new char[] { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', '\\', ':', ';', '"', '\'', '<', '>', ',', '.', '?', '/' };
+        private readonly char[] numberCharcters = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        public bool ValidateNameSurname(string nameSurname = null)
+        {
+            if (nameSurname.Length < 5)
+                return false;
+
+            return true;
+        }
+
+        public bool ValidateEmail(string email)
+        {
+            return email.Contains("@") && email.Length > 5;
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            int specialCharactersCount = 0;
+            int numberCharactersCount = 0;
+            int upperCaseCount = 0;
+
+            for (int i = 0; i < specialCharacters.Length; i++)
+            {
+                if (password.Contains(specialCharacters[i]))
+                    specialCharactersCount++;
+            }
+
+            for (int i = 0; i < numberCharcters.Length; i++)
+            {
+                if (password.Contains(numberCharcters[i]))
+                    numberCharactersCount++;
+            }
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                if(char.IsUpper(password[i]))
+                    upperCaseCount++;
+            }
+
+            if (specialCharactersCount < 1 || numberCharactersCount < 1 || password.Length < 8 || upperCaseCount < 2)
+                return false;
+            
+            return true;
+        }
+    }
+
     public class AuthService
     {
         private readonly UserManager _userManager;
@@ -92,4 +180,6 @@ namespace WeatherPlusZero
             _userManager = userManager;
         }
     }
+
+    public enum ValidateType { All, NameSurname, Email, Password }
 }
--- WeatherPlusZero/UserSessionWindow.xaml.cs
+++ WeatherPlusZero/UserSessionWindow.xaml.cs
@@ -17,59 +17,153 @@ namespace WeatherPlusZero
 {
     public partial class UserSessionWindow : Window
     {
-        UserManager userManager;
+        private readonly UserManager _userManager;
+        private readonly Dictionary<Panels, Border> panelBorders;
+        private readonly BitmapImage tickIcon;
+        private readonly BitmapImage errorIcon;
 
         public UserSessionWindow()
         {
-            userManager = new UserManager();
+            _userManager = new UserManager();
+            tickIcon = new BitmapImage(new Uri("Images/TickIcon.png", UriKind.Relative));
+            errorIcon = new BitmapImage(new Uri("Images/ErrorIcon.png", UriKind.Relative));
+
             InitializeComponent();
+
+            panelBorders = new Dictionary<Panels, Border>
+            {
+                { Panels.Login, LoginPanelBackgroundBorder },
+                { Panels.Register, RegisterPanelBackgroundBorder },
+                { Panels.Forgot, ForgotPanelBackgroundBorder },
+                { Panels.EmailVerification, EmailVerificationPanelBackgroundBorder },
+                { Panels.ChangePassword, ChangePasswordPanelBackgroundBorder }
+            };
 
             ApplicationStart();
         }
 
-        private void ApplicationStart()
-        {
-            LoginPanelBackgroundBorder.Visibility = Visibility.Visible;
-
-            RegisterPanelBackgroundBorder.Visibility = Visibility.Collapsed;
-
-            EmailVerificationPanelBackgroundBorder.Visibility = Visibility.Collapsed;
-
-            ChangePasswordPanelBackgroundBorder.Visibility = Visibility.Collapsed;
-        }
+        private void ApplicationStart() => PanelTransition(Panels.Login);
 
         private void LogInClickButton(object sender, RoutedEventArgs e)
         {
-            User user = new User
-            {
-                email = Login_EmailTextBox.Text,
-                password = Login_PasswordTextBox.Password
-            };
-
-            userManager.LogIn(user);
+            
         }
 
-        private void RegisterClickButton(object sender, RoutedEventArgs e)
+        private void Login_EmailTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            User user = new User
-            {
-                email = Register_EmailTextBox.Text,
-                password = Register_PasswordTextBox.Password
-            };
-
-            userManager.Register(user);
+            bool iconStatus = !_userManager.ValidateFieldsContinuously(ValidateType.Email, Login_EmailTextBox.Text);
+            TextBoxInputIcon(Panels.Login, TextBoxInputIconType.Email, iconStatus);
         }
 
-        private void RegisterPanelClickButton(object sender, RoutedEventArgs e)
+        private void Login_PasswordTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            RegisterPanelBackgroundBorder.Visibility = Visibility.Visible;
-            LoginPanelBackgroundBorder.Visibility = Visibility.Collapsed;
+            bool iconStatus = !_userManager.ValidateFieldsContinuously(ValidateType.Password, Login_PasswordTextBox.Text);
+            TextBoxInputIcon(Panels.Login, TextBoxInputIconType.Password, iconStatus);
         }
+
+        private void Register_NamesurnameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool iconStatus = !_userManager.ValidateFieldsContinuously(ValidateType.NameSurname, Register_NamesurnameTextBox.Text);
+            TextBoxInputIcon(Panels.Register, TextBoxInputIconType.Namesurname, iconStatus);
+        }
+
+        private void Register_EmailTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool iconStatus = !_userManager.ValidateFieldsContinuously(ValidateType.Email, Register_EmailTextBox.Text);
+            TextBoxInputIcon(Panels.Register, TextBoxInputIconType.Email, iconStatus);
+        }
+
+        private void Register_PasswordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool iconStatus = !_userManager.ValidateFieldsContinuously(ValidateType.Password, Register_PasswordTextBox.Text);
+            TextBoxInputIcon(Panels.Register, TextBoxInputIconType.Password, iconStatus);
+        }
+
+        private void Forgot_NamesurnameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool iconStatus = !_userManager.ValidateFieldsContinuously(ValidateType.NameSurname, Forgot_NamesurnameTextBox.Text);
+            TextBoxInputIcon(Panels.Forgot, TextBoxInputIconType.Namesurname, iconStatus);
+        }
+
+        private void Forgot_EmailTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool iconStatus = !_userManager.ValidateFieldsContinuously(ValidateType.Email, Forgot_EmailTextBox.Text);
+            TextBoxInputIcon(Panels.Forgot, TextBoxInputIconType.Email, iconStatus);
+        }
+
+        private void EmailVerification_EmailKeyCodeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            return;
+        }
+
+        private void ChangePassword_ChangePasswordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool iconStatus = !_userManager.ValidateFieldsContinuously(ValidateType.Password, ChangePassword_ChangePasswordTextBox.Text);
+            TextBoxInputIcon(Panels.ChangePassword, TextBoxInputIconType.Password, iconStatus);
+        }
+
+        private void RegisterClickButton(object sender, RoutedEventArgs e) 
+            => _userManager.Register(new User { email = Register_EmailTextBox.Text, password = Register_PasswordTextBox.Text });
+
+        private void RegisterPanelClickButton(object sender, RoutedEventArgs e) 
+            => PanelTransition(Panels.Register);
 
         private void LogInPanelClickButton(object sender, RoutedEventArgs e)
+            => PanelTransition(Panels.Login);
+
+        private void ForgotPasswordClickButton(object sender, RoutedEventArgs e) 
+            => PanelTransition(Panels.Forgot);
+
+        private void ForgotClickButton(object sender, RoutedEventArgs e) 
+            => PanelTransition(Panels.EmailVerification);
+
+        private void EmailVerificationClickButton(object sender, RoutedEventArgs e) 
+            => PanelTransition(Panels.ChangePassword);
+
+        private void ChangePasswordClickButton(object sender, RoutedEventArgs e) 
+            => PanelTransition(Panels.Login);
+
+        private void PanelTransition(Panels panelToShow)
         {
-            LoginPanelBackgroundBorder.Visibility = Visibility.Visible;
-            RegisterPanelBackgroundBorder.Visibility = Visibility.Collapsed;
+            foreach (var panel in panelBorders)
+            {
+                panel.Value.Visibility = panel.Key == panelToShow ? Visibility.Visible : Visibility.Collapsed;
+            }
         }
+
+        private void TextBoxInputIcon(Panels panel, TextBoxInputIconType iconType, bool iconStatus)
+        {
+            Image statusIcon = GetStatusIcon(panel, iconType);
+
+            if (iconStatus)
+            {
+                statusIcon.Source = tickIcon;
+                return;
+            }
+            statusIcon.Source = errorIcon;
+            return;
+        }
+        
+        private Image GetStatusIcon(Panels panel, TextBoxInputIconType iconType)
+        {
+            switch (panel, iconType)
+            {
+                case (Panels.Login, TextBoxInputIconType.Email): return Login_EmailStatusIconImage;
+                case (Panels.Login, TextBoxInputIconType.Password): return Login_PasswordStatusIconImage;
+                case (Panels.Register, TextBoxInputIconType.Email): return Register_EmailStatusIconImage;
+                case (Panels.Register, TextBoxInputIconType.Password): return Register_PasswordStatusIconImage;
+                case (Panels.Register, TextBoxInputIconType.Namesurname): return Register_NamesurnameStatusIconImage;
+                case (Panels.Forgot, TextBoxInputIconType.Email): return Forgot_EmailStatusIconImage;
+                case (Panels.ChangePassword, TextBoxInputIconType.Password): return ChangePassword_StatusIconImage;
+                default: return null;
+            }
+        }
+
+        public enum Panels { Login, Register, Forgot, EmailVerification, ChangePassword }
+
+        public enum TextBoxInputIconStatus { Error, Success }
+
+        // todo: TextBoxInputIconStatus ile ValidateType aynı enumlardır. O yüzden birleştirilebilir.
+        public enum TextBoxInputIconType { Email, Password, Namesurname }
     }
 }
